039b8e5f6937759be09f2c32caa481602ac3269a
Jiri Furst <Jiri.Furst@gmail.com>
2018-06-18 22:08:50 +0200

    Corrected time step in mySonicLiquidDyMFoam

--- mySolvers/mySonicLiquidFoam/mySonicLiquidDyMFoam/mySonicLiquidDyMFoam.C
+++ mySolvers/mySonicLiquidFoam/mySonicLiquidDyMFoam/mySonicLiquidDyMFoam.C
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 
     Info<< "\nStarting time loop\n" << endl;
 
-    while (runTime.loop())
+    while (runTime.run())
     {
         #include "readControls.H"
  
